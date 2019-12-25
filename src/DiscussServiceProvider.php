83bbe05ec0c495cb26f2e4069a78fe816230b8a2
oleksandr-bolshov <o.bolshov@student.csn.khai.edu>
2019-12-25 17:45:46 +0200

    fix bugs [ci skip]

--- src/DiscussServiceProvider.php
+++ src/DiscussServiceProvider.php
@@ -6,7 +6,6 @@ namespace Apathy\Discuss;
 
 use Apathy\Discuss\Contracts\ChatService as ChatServiceContract;
 use Apathy\Discuss\Contracts\FollowingService as FollowingServiceContract;
-use Apathy\Discuss\Contracts\ImageService as ImageServiceContract;
 use Apathy\Discuss\Contracts\LikeService as LikeServiceContract;
 use Apathy\Discuss\Contracts\ListService as ListServiceContract;
 use Apathy\Discuss\Contracts\MessageService as MessageServiceContract;
@@ -15,7 +14,6 @@ use Apathy\Discuss\Contracts\TweetService as TweetServiceContract;
 use Apathy\Discuss\Contracts\UserService as UserServiceContract;
 use Apathy\Discuss\Services\ChatService;
 use Apathy\Discuss\Services\FollowingService;
-use Apathy\Discuss\Services\ImageService;
 use Apathy\Discuss\Services\LikeService;
 use Apathy\Discuss\Services\ListService;
 use Apathy\Discuss\Services\MessageService;
@@ -30,7 +28,6 @@ final class DiscussServiceProvider extends ServiceProvider
     {
         $this->app->bind(UserServiceContract::class, UserService::class);
         $this->app->bind(TweetServiceContract::class, TweetService::class);
-        $this->app->bind(ImageServiceContract::class, ImageService::class);
         $this->app->bind(PollServiceContract::class, PollService::class);
         $this->app->bind(LikeServiceContract::class, LikeService::class);
         $this->app->bind(FollowingServiceContract::class, FollowingService::class);
